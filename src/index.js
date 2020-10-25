15f19e8d015b73d075759870d8cdd59fc69e70c3
Anna Gudinskaya <ann.gudin@gmail.com>
2020-10-25 23:35:22 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0) {
+       let num = n * (-1)
+        return num.toString().split('').reverse().join('')
+    } 
+    return n.toString().split('').reverse().join('')
 }
+// console.log(reverse (-123))
